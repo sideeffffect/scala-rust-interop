0ef34d101169ada829c688f3a26dc15515e4a608
Ondřej Pelech <ondrej.pelech@avast.com>
2021-05-04 16:37:05 +0200

    init 2

---
+++ src/main/scala/com/github/sideeffffect/scalarustinterop/Main.scala
@@ -0,0 +1,8 @@
+package com.github.sideeffffect.scalarustinterop
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("hello")
+  }
+}
---
+++ src/test/scala/com/github/sideeffffect/scalarustinterop/MainSpec.scala
@@ -0,0 +1,7 @@
+package com.github.sideeffffect.scalarustinterop
+
+object MainSpec {
+  def main(args: Array[String]): Unit = {
+    println("test")
+  }
+}
