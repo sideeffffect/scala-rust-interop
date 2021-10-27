497af18f7e754a2a7b223a10bc8ddb0fe0eb1d3b
Ondra Pelech <ondra.pelech@gmail.com>
2021-10-27 16:04:55 +0200

    catch_unwind and throw as RuntimeException

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,12 +1,12 @@
+use std::panic;
+
 // This is the interface to the JVM that we'll call the majority of our
 // methods on.
 use jni::JNIEnv;
-
 // These objects are what you should use as arguments to your native
 // function. They carry extra lifetime information to prevent them escaping
 // this context and getting used after being GC'd.
 use jni::objects::JObject;
-
 use jni::sys::jint;
 
 // This keeps Rust from "mangling" the name and making it unique for this
@@ -17,8 +17,19 @@ pub extern "system" fn Java_com_github_sideeffffect_scalarustinterop_Adder_plus(
   object: JObject,
   term: jint,
 ) -> jint {
-  let base = env.get_field(object, "base", "I").unwrap().i().unwrap();
-  println!("Printing from rust library. base: {}", base);
-  println!("Printing from rust library. term: {}", term);
-  base + term
+  let result = panic::catch_unwind(|| {
+    let base = env.get_field(object, "base", "I").unwrap().i().unwrap();
+    println!("Printing from rust library. base: {}", base);
+    println!("Printing from rust library. term: {}", term);
+    base + term
+  });
+  result.unwrap_or_else(|e| {
+    let description = e
+      .downcast_ref::<String>()
+      .map(|e| &**e)
+      .or_else(|| e.downcast_ref::<&'static str>().copied())
+      .unwrap_or("Unknown error in native code.");
+    env.throw_new("java/lang/RuntimeException", description).unwrap();
+    -1 // some value to satisfy type checker, it won't be used
+  })
 }
